00641dd7cbbbc2cd803301f5401416aa989896ea
Pawani328 <pawanih.22@cse.mrt.ac.lk>
2024-10-29 14:08:13 +0530

    changes to homepage1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
         </AuthProvider>
         <Footer/>
       </body>
+      
     </html>
   );
 }
--- src/components/ui/text-generate-effect.tsx
+++ src/components/ui/text-generate-effect.tsx
@@ -4,45 +4,52 @@ import { motion, stagger, useAnimate } from "framer-motion";
 import { cn } from "@/lib/utils";
 
 export const TextGenerateEffect = ({
-  words,
   className,
   filter = true,
-  duration = 0.5,
+  duration = 0.7,
 }: {
-  words: string;
   className?: string;
   filter?: boolean;
   duration?: number;
 }) => {
   const [scope, animate] = useAnimate();
-  let wordsArray = words.split(" ");
+
+  // Define the lines of text
+  const textLines = [
+    "Discover New Destinations",
+    "Book with Ease",
+    "Begin Your Adventure Today",
+  ];
+
   useEffect(() => {
     animate(
       "span",
       {
-        opacity: 1,
+        opacity: 2,
         filter: filter ? "blur(0px)" : "none",
       },
       {
         duration: duration ? duration : 1,
-        delay: stagger(0.2),
+        delay: stagger(0.3),
       }
     );
   }, [scope.current]);
 
-  const renderWords = () => {
+  const renderTextLines = () => {
     return (
-      <motion.div ref={scope}>
-        {wordsArray.map((word, idx) => {
+      <motion.div ref={scope} className="flex flex-col items-center">
+        {textLines.map((line, idx) => {
           return (
             <motion.span
-              key={word + idx}
-              className="dark:text-green text-#94a3b8 opacity-0"
+              key={line + idx}
+              className="opacity-0"
               style={{
                 filter: filter ? "blur(10px)" : "none",
               }}
             >
-              {word}{" "}
+              <span className="text-8xl leading-snug tracking-wide text-white">
+                {line}
+              </span>
             </motion.span>
           );
         })}
@@ -51,10 +58,16 @@ export const TextGenerateEffect = ({
   };
 
   return (
-    <div className={cn("font-bold", className)}>
+    <div className={cn("font-bold text-center", className)} style={{ fontFamily: "Arial, sans-serif" }}>
       <div className="mt-4">
-        <div className=" dark:text-white text-gray-900 text-6xl leading-snug tracking-wide">
-          {renderWords()}
+        <div
+          className="dark:text-gray-700 text-white"
+          style={{
+            fontFamily: "Playfair Display", 
+            textShadow: "3px 3px 7px rgba(1, 1, 1, 0.5)", // Add shadow effect here
+          }}
+        >
+          {renderTextLines()}
         </div>
       </div>
     </div>
