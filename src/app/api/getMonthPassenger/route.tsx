e938f3cfd919aa24521022a7efb2c8586f3ceb86
MethmiRathnayaka <155360000+MethmiRathnayaka@users.noreply.github.com>
2024-10-29 18:59:33 +0530

    next apis

--- src/app/api/getMonthPassenger/route.tsx
+++ src/app/api/getMonthPassenger/route.tsx
@@ -1,39 +1,32 @@
 import { NextRequest, NextResponse } from "next/server";
 import mysql from "mysql2/promise";
 import { GetDBSettings } from "@/sharedCode/common";
+
 let connectionParams = GetDBSettings();
-interface passengers {
+
+interface PassengerCount {
+  month: string;
   passenger_count: number;
   year: string;
-  month: string;
 }
 
 export async function GET(request: NextRequest) {
   try {
     const connection = await mysql.createConnection(connectionParams);
 
-    // Fetch passengers under and over 18 years old
-    const [passengers]: any[] = await connection.execute(
-      `SELECT 
-    MONTHNAME(flightschedule.date) AS month, 
-    COUNT(passenger.passenger_id) AS desktop, 
-    YEAR(flightschedule.date) AS mobile
-FROM 
-    flight 
-    JOIN bookings USING(flight_id) 
-    JOIN flightschedule USING(flightSchedule_id) 
-    JOIN passenger USING(booking_id)
-GROUP BY mobile, month
-HAVING mobile = YEAR(CURDATE())
-ORDER BY MIN(flightschedule.date);
-`
+    // Call the stored procedure to fetch passenger counts
+    const [passengerCounts]: any[] = await connection.execute(
+      'CALL GetPassengerCountsByYear()'
     );
 
-    console.log(passengers);
-    console.log(passengers[0].mobile);
+    console.log(passengerCounts);
+    
+    // If passengerCounts is not empty, extract the year from the first record
+    const year = passengerCounts.length > 0 ? passengerCounts[0].year : null;
+
     return NextResponse.json({
-      year: passengers[0].mobile,
-      passenger: passengers,
+      year: year,
+      passenger: passengerCounts,
     });
   } catch (err) {
     console.log("ERROR: API - ", (err as Error).message);
